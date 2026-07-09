9efe32e5794de35a6e22bce9d62f10d6efd23fa3
zaidhassan168 <zaidhassan168@outlook.com>
2026-07-09 14:54:05 +0500

    Bump kit to v1.1.1 (fix help crash)

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -1,8 +1,8 @@
 class Kit < Formula
   desc "Kubernetes Interactive Toolkit for Azure Kubernetes Service (AKS)"
   homepage "https://github.com/zaidhassan168/aks-tools"
-  url "https://github.com/zaidhassan168/aks-tools/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "7570b10cce4494b96c4a5974a3fd2f045276601d3db4f10848be16651e008f18"
+  url "https://github.com/zaidhassan168/aks-tools/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "d343c82a9e1ecc18c25bbdafafb94f39f4ddfa340b62b4442a76f8d7b7725ed2"
   license "MIT"
 
   depends_on "azure-cli"
